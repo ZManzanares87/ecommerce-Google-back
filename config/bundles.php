f31759d96ae6df5a70f0265309ed7bc7cfd16400
JUANAN2019 <garciagalindo@telefonica.net>
2023-09-01 15:38:57 +0200

    para cambiarme de rama

--- config/bundles.php
+++ config/bundles.php
@@ -13,4 +13,6 @@ return [
     SymfonyCasts\Bundle\VerifyEmail\SymfonyCastsVerifyEmailBundle::class => ['all' => true],
     Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
     Lexik\Bundle\JWTAuthenticationBundle\LexikJWTAuthenticationBundle::class => ['all' => true],
+    EasyCorp\Bundle\EasyAdminBundle\EasyAdminBundle::class => ['all' => true],
+    Twig\Extra\TwigExtraBundle\TwigExtraBundle::class => ['all' => true],
 ];
