d08c846f7760c488f301ea9a6a7eb001ec548b58
JUANAN2019 <garciagalindo@telefonica.net>
2023-09-07 10:41:28 +0200

    subida coprobacion

---
+++ src/Controller/ApiCategoryController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Controller;
+use App\Repository\CategoryRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ApiCategoryController extends AbstractController
+{
+    #[Route('/api/categories', name: 'app_api_categories_index',methods: ['GET'])]
+    public function index(CategoryRepository $categoryRepository): Response
+    {
+        $categories = $categoryRepository->findAll();
+
+        $data = [];
+        foreach ($categories as $category) {
+            $data[] = [
+                'name' => $category->getName(),
+                
+            ];
+        }
+
+        return $this->json($data);
+    }
+}
