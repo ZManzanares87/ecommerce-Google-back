edb20bd79f17b5b4a8755df0fb6f2ca9d3027d19
JUANAN2019 <garciagalindo@telefonica.net>
2023-09-12 13:45:38 +0200

    limpiado .env y subo nuestra bbdd de local no la de servidor

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -12,9 +12,7 @@ class SecurityController extends AbstractController
     #[Route(path: '/login', name: 'app_login')]
     public function login(AuthenticationUtils $authenticationUtils): Response
     {
-        // if ($this->getUser()) {
-        
-        // }
+       
 
         // get the login error if there is one
         $error = $authenticationUtils->getLastAuthenticationError();
